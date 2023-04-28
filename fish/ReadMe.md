e67f52818d99debb5ae52a6b3fd5969b47bfbe41
Arfan <shaifarfan08@gmail.com>
2023-04-28 22:46:24 +0600

    update fish settings

---
+++ fish/ReadMe.md
@@ -0,0 +1,6 @@
+## My settings for fish shell
+
+- Fish Shell: https://fishshell.com/
+- Oh My Fish: https://github.com/oh-my-fish/oh-my-fish
+- Theme(omf-bobthefish): https://github.com/oh-my-fish/theme-bobthefish
+- NVM(nvm.fish): https://github.com/jorgebucaran/nvm.fish
--- fish/config.fish
+++ fish/config.fish
@@ -2,16 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+#node version (set --universal nvm_default_version your_version)
+nvm use $nvm_default_version
+
 #these are the settings for the theme
 #them theme i am currently is called "bobthefish"(https://github.com/oh-my-fish/theme-bobthefish)
 set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
 set -g theme_display_date no
 set -g theme_display_node yes
 set -g theme_display_git_default_branch yes
 set -g theme_project_dir_length 3
 set -g theme_title_display_process yes
-set -g theme_newline_prompt '$ '
 
+#fish key bindings
 fish_vi_key_bindings
 alias mongo_start="sudo systemctl start mongod"
 alias mongo_status="sudo systemctl status mongod"
